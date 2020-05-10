481ca3f635aba8fa8dfb1bc76e6b67275970636b
Imdad15 <imdad92@live.com>
2020-05-10 16:26:31 +0800

    set rinkeby deployment environment

--- KnowYourAvacados/truffle-config.js
+++ KnowYourAvacados/truffle-config.js
@@ -1,9 +1,20 @@
+require('dotenv').config();
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = process.env["MNEMONIC"];
+var infuraKey = process.env["INFURA_KEY"];
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,
+        gas: 4500000,
+        gasPrice: 10000000000
+    },
   }
 };
